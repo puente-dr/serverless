f61b225912ad5365fb85748c1b104cfa15518054
Joseph McCombs <jamccomb92@gmail.com>
2020-07-10 17:05:03 -0400

    Initial working lambda to s3 with json format

--- google/sheets-python-communitydata-endpoint/lambda.py
+++ google/sheets-python-communitydata-endpoint/lambda.py
@@ -1,10 +1,29 @@
 import json
+import boto3
+import csv
 import smtplib
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
+s3 = boto3.client('s3')
+
 
 def lambda_handler(event, context):
+    worksheet_name = event['worksheet_name']
+    sheet_name = event['sheet_name']
+    key = event['key']
+    print("Worksheet: "+worksheet_name+" Sheet Name: " +
+          sheet_name+" bucket: "+bucket_name+" key: "+key)
+
+    upload_json(event)
+
+    return {
+        'status_code': 200,
+        'body': 'File successuflly uploaded to s3.'
+    }
+
+
+def upload_json(event):
     # use creds to create a client to interact with the Google Drive API
     scope = ['https://spreadsheets.google.com/feeds',
              'https://www.googleapis.com/auth/drive']
@@ -14,10 +33,16 @@ def lambda_handler(event, context):
 
     # Find a workbook by name and open the first sheet
     # Make sure you use the right name here.
-    spr = client.open("")
-    sheet = spr.worksheet('content')
+    spr = client.open(event['worksheet_name'])
+    sheet = spr.worksheet(event['sheet_name'])
 
-    # Extract and print all of the values
+    # Extract all of the values
     records = sheet.get_all_records()
-    for record in records:
-        print(record)
+    print(records)
+
+    # Upload json to s3
+    s3.put_object(
+        Body=str(json.dumps(records)),
+        Bucket=event['bucket_name'],
+        Key=event['key']
+    )
