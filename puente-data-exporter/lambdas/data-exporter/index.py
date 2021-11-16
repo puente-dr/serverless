d01baf8ce109fc10d4df83bbabb4e6af63d37506
Joseph McCombs <jamccomb92@gmail.com>
2021-11-15 22:19:48 -0500

    chore: adding original lambda code back

--- puente-data-exporter/lambdas/data-exporter/index.py
+++ puente-data-exporter/lambdas/data-exporter/index.py
@@ -3,51 +3,39 @@ import os
 
 sys.path.append(os.path.join(os.path.dirname(__file__)))
 
-# from mainRecords import mainRecords
-# from envHealth import envHealth
-# from evalMedical import evalMedical
-# from restCall import restCall
+from mainRecords import mainRecords
+from envHealth import envHealth
+from evalMedical import evalMedical
+from restCall import restCall
 
-#import json
-
-import pandas as pd
 import json
 
-import boto3 #don't need this in lambda, but might to run locally idk
-
-
 def handler(event, context=None):
-  # f = open(event, )
-  # event_json = json.load(f)
-  print(event)
-  print(type(event))
   event = event["queryStringParameters"]
+
   bucket_name = event["bucket_name"]
   survey_org = event["surveyingOrganization"]
   specifier = event["specifier"]
-  # survey_org="Puente"
-  # specifier = "SurveyData"
   print(survey_org)
   print(specifier)
 
-  # data = restCall(specifier, survey_org)
+  data = restCall(specifier, survey_org)
 
-  # if specifier == "SurveyData":
-  #   response = mainRecords(data, survey_org, bucket_name)
-  # elif specifier == "HistoryEnvironmentalHealth":
-  #   response = envHealth(data, survey_org, bucket_name)
-  # elif specifier == "EvaluationMedical":
-  #   response = evalMedical(data, survey_org, bucket_name)
-  # else:
-  print("Pick a valid specifier")
-  response = {
+  if specifier == "SurveyData":
+    response = mainRecords(data, survey_org, bucket_name)
+  elif specifier == "HistoryEnvironmentalHealth":
+    response = envHealth(data, survey_org, bucket_name)
+  elif specifier == "EvaluationMedical":
+    response = evalMedical(data, survey_org, bucket_name)
+  else:
+    response = {"message": "Oops, look like you didnt inlude a valid specifier..."}
+  
+  return {
     "headers": {"Access-Control-Allow-Origin":"*"},
     "statusCode": 200,
     "isBase64Encoded": False,
-    "body": json.dumps({"message": "Oops, looks like you didnt include a valid specifier.."})
+    "body": json.dumps(response)
   }
-
-  return response
 
 # if __name__ == '__main__':
 #     globals()[sys.argv[1]](sys.argv[2])
