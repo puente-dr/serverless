183e63b57a4f00bf00fee75ca7272e2ffdef9282
awalsh272 <walsha2.72@gmail.com>
2021-12-16 20:26:18 -0500

    Revert "Andy dev"

--- puente-data-exporter/lambdas/data-exporter/libs/restCall.py
+++ puente-data-exporter/lambdas/data-exporter/libs/restCall.py
@@ -1,8 +1,6 @@
-from typing import Dict
 import requests
 import json
 from pandas import json_normalize
-import pandas as pd
 
 import sys
 import os
@@ -11,87 +9,74 @@ sys.path.append(os.path.join(os.path.dirname(__file__)))
 
 import secretz
 
-def restCall(specifier, survey_org, custom_form_id, url="https://parseapi.back4app.com/classes/"):
-    """
+def restCall(specifier, survey_org, url="https://parseapi.back4app.com/classes/"):
+    """Sample pure Lambda function
+
     Parameters
     ----------
-    specificer: string, required
-        Parse model to query
+    event: dict, required
+        API Gateway Lambda Proxy Input Format
 
-    survey_org: string, required
-        Organization to query results for
+        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
 
-    custom_form_id: string, not required
-        Custom form object id 
+    context: object, required
+        Lambda Context runtime methods and attributes
 
-    url: string, required
-        Parse endpoint
+        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
+
     Returns
     ------
-    Pandas dataframe with query results
+    API Gateway Lambda Proxy Output Format: dict
 
+        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
     """
+
+    # try:
+    #     ip = requests.get("http://checkip.amazonaws.com/")
+    # except requests.RequestException as e:
+    #     # Send some context about this error to Lambda Logs
+    #     print(e)
+
+    #     raise e
+    # def restCall(specifier, url="https://parseapi.back4app.com/classes/"):
+    # """Only difference between calls is the specifier
+    #    This function makes a REST Get call to Back4App, returning a pandas df"""
+
+    # url = event["url"]
+    # specifier = event["specifier"]
+
+    # url="https://parseapi.back4app.com/classes/"
+    # specifier = "SurveyData"
 
     # combine base url with specifier
     combined_url = url + specifier
-    params = dict()
 
-    if specifier != 'FormResults' and specifier != 'FormAssetResults':
-        params = {
-            "order": "-updatedAt", "limit": 200000, "where":{
-                json.dumps({
-                    "surveyingOrganization": {
-                        "$in": [survey_org]
-                    }
-                })
+    # REST stuff to make sure you get data
+    #HAD PROBLEMS WITH PAYLOAD SIZE, LOWERING LIMIT FIXED
+    #increase payload?
+    params = {
+      "order": "-updatedAt", "limit": 200000, "where":{
+          json.dumps({
+            "surveyingOrganization": {
+                "$in": [survey_org]
             }
-        }
-    else:
-        # custom forms need to ensure that the correct custom form results are returned
-        params = {
-            "order": "-updatedAt", "limit": 200000, "where":{
-                json.dumps({
-                    "surveyingOrganization": {
-                        "$in": [survey_org]
-                    },
-                    "formSpecificationsId": {
-                        "$in": [custom_form_id]
-                    }
-                })
-            }
-        }
-
+          })
+      }
+}
 
     headers = {
         "Content-Type": "application/json",
         "X-Parse-Application-Id": secretz.APP_ID,
         "X-Parse-REST-API-Key": secretz.REST_API_KEY,
     }
-    
+
+    # GET operation
     response = requests.get(combined_url, params=params, headers=headers)
 
+    # turn it into json
     json_obj = response.json()
+
     # normalize (ie flatten) data, turns it into a pandas df
     normalized = json_normalize(json_obj["results"])
-
-    # join non surveyData forms to surveyData
-    if specifier != 'SurveyData' and specifier != 'Assets':
-        combined_url = url + 'SurveyData' if specifier != 'FormAssetResults' else url + 'Assets'
-        params = {
-            "order": "-updatedAt", "limit": 200000, "where":{
-                json.dumps({
-                    "surveyingOrganization": {
-                        "$in": [survey_org]
-                    }
-                })
-            }
-        }
-        response_survey_data = requests.get(combined_url, params=params, headers=headers)
-        json_obj_survey_data = response_survey_data.json()
-        normalized_survey_data = json_normalize(json_obj_survey_data["results"])
-        normalized = normalized.rename(columns= {'objectId':specifier+'Id','client.objectId':'objectId','surveyingUser':'surveyingUserSupplementary'})
-        merged_df = pd.merge(normalized_survey_data, normalized, on="objectId")
-        return merged_df
-
 
     return normalized
