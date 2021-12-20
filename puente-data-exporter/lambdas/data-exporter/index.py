1ab0a3625609e8464a1d458772c1be571aa6e491
awalsh272 <walsha2.72@gmail.com>
2021-12-20 17:51:14 -0600

    Adding the cleaning functions back in
    Uncomment out the cleaning functions, see if they work on AWS. Probably won't because I suck at coding

--- puente-data-exporter/lambdas/data-exporter/index.py
+++ puente-data-exporter/lambdas/data-exporter/index.py
@@ -7,6 +7,7 @@ sys.path.append(os.path.join(os.path.dirname(__file__)))
 from libs.envHealth import envHealth
 from libs.evalMedical import evalMedical
 from libs.mainRecords import mainRecords
+from libs.vitals import vitals
 from libs.restCall import restCall
 from libs.utils import write_csv_to_s3
 
@@ -33,16 +34,16 @@ def handler(event, context=None):
   url = write_csv_to_s3(data, s3_bucket_key)
 
  
-  # if specifier == "SurveyData":
-  #   response = mainRecords(data, survey_org)
-  # elif specifier == "HistoryEnvironmentalHealth":
-  #   response = envHealth(data, survey_org)
-  # elif specifier == "EvaluationMedical":
-  #   response = evalMedical(data, survey_org)
-  # elif specifier == "Vitals":
-  #   response = vitals(data, survey_org)
-  # else:
-  #   response = {"message": "Oops, look like you didnt inlude a valid specifier..."}
+  if specifier == "SurveyData":
+    response = mainRecords(data, survey_org)
+  elif specifier == "HistoryEnvironmentalHealth":
+    response = envHealth(data, survey_org)
+  elif specifier == "EvaluationMedical":
+    response = evalMedical(data, survey_org)
+  elif specifier == "Vitals":
+    response = vitals(data, survey_org)
+  else:
+    response = {"message": "Oops, look like you didnt inlude a valid specifier..."}
 
 
   response = {
