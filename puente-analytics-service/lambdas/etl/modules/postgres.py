c97d56c99d64a534019e65dc71d4cf074746072b
Hope Tambala <hopetambala@outlook.com>
2023-05-03 19:25:33 -0400

    Update postgres.py

--- puente-analytics-service/lambdas/etl/modules/postgres.py
+++ puente-analytics-service/lambdas/etl/modules/postgres.py
@@ -7,17 +7,6 @@ PG_DATABASE = os.environ.get('PG_DATABASE')
 PG_USERNAME = os.environ.get('PG_USERNAME')
 PG_PASSWORD = os.environ.get('PG_PASSWORD')
 
-# DATABASE_URI = 'postgresql://{user}:{password}@{host}:{port}/{database}'.format(
-#     user=PG_USERNAME, password=PG_PASSWORD, host=PG_HOST, port=PG_PORT, database=PG_DATABASE)
-# engine = create_engine(DATABASE_URI)
-# metadata = MetaData()
-
-
-# def createSession():
-#     # DATABASE_URI = 'postgresql://user:password@host:port/database'
-#     Session = sessionmaker(bind=engine)
-#     return Session
-
 def connection():
     conn = psycopg2.connect(
         host=PG_HOST,
